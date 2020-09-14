e158ab2953464ec46d969b70927bd90db5ceb7f6
stormrubio3000 <storm-3000@hotmail.com>
2020-09-14 15:55:51 -0500

    Add project files.

---
+++ CircularBuffer.Test/UnitTest1.cs
@@ -0,0 +1,68 @@
+using System;
+using Xunit;
+
+namespace CircularBuffer.Test
+{
+    public class CircularBufferTests
+    {
+        [Fact]
+        public void CircularBufferCreatePass()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferCreateFail()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferAddPass()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferAddFail()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferOverwritePass()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferOverwriteFail()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferReadNextPass()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferReadNextFail()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferReadOldestPass()
+        {
+
+        }
+
+        [Fact]
+        public void CircularBufferReadOldestFail()
+        {
+
+        }
+    }
+}
